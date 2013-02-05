ea2a544cf1519049e50757a16a541e621c9cd91f
Ansinger <ansinger@gmail.com>
2013-02-05 15:46:33 -0500

    Finished healthcare landing page

--- js/script.js
+++ js/script.js
@@ -1,8 +1,29 @@
 $(function() {
-	$("#features .slider-image .screens").cycle({
-		fx: 'venioSlider',
-		delay: 1000,
-		pager: $('#pager')
+	$('.carousel').carousel({
+		pause: 'mouseenter',
+		interval: 5000
+	}).bind('slide', function(event) {
+		console.log(event);
+		var elm = $(event.relatedTarget);
+		var index = elm.parent().children().index(elm);
+		
+		$('#pager a.active').removeClass('active');		
+		
+		var pager_elem = $('#pager a')[index];
+		$(pager_elem).addClass('active');
+	});
+	
+	$('#pager a').click(function(event) {
+		event.preventDefault();
+		var elm = $(event.currentTarget);
+		
+		var index = elm.parent().children().index(elm);
+		
+		$('.carousel').carousel(index);
+		$('#pager a.active').removeClass('active');		
+		
+		var pager_elem = $('#pager a')[index];
+		$(pager_elem).addClass('active');
 	});
 });
 
