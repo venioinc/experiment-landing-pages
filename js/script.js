573f37ce1663b82026e57a2b416011c5452ba014
Karim El Rabiey <karim@ven.io>
2013-02-06 18:21:25 -0500

    fitness landing page

--- js/script.js
+++ js/script.js
@@ -1,8 +1,44 @@
+analytics.initialize({
+	'Google Analytics' : 'UA-38270530-1',
+	'Mixpanel' : {
+					token:'9be8f28935f630ac2e528b939878581d',
+				}
+});
+
 $(function() {
-	$("#features .slider-image .screens").cycle({
-		fx: 'venioSlider',
-		delay: 1000,
-		pager: $('#pager')
+	var link = $('.convert');
+
+	analytics.trackLink(contact, 'Clicked to Convert');
+	
+	var contact = $('#contact_us');
+	
+	analytics.trackLink(contact, 'Clicked to Contact');
+	
+	$('.carousel').carousel({
+		pause: null,
+		interval: 15000
+	}).bind('slide', function(event) {
+		console.log(event);
+		var elm = $(event.relatedTarget);
+		var index = elm.parent().children().index(elm);
+		
+		$('#pager a.active').removeClass('active');		
+		
+		var pager_elem = $('#pager a')[index];
+		$(pager_elem).addClass('active');
+	});
+	
+	$('#pager a').click(function(event) {
+		event.preventDefault();
+		var elm = $(event.currentTarget);
+		
+		var index = elm.parent().children().index(elm);
+		
+		$('.carousel').carousel(index);
+		$('#pager a.active').removeClass('active');		
+		
+		var pager_elem = $('#pager a')[index];
+		$(pager_elem).addClass('active');
 	});
 });
 
