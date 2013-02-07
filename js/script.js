74011326f0d317a9e6ad40397b36cc335d7a5481
Nima Gardideh <nima@ven.io>
2013-02-07 12:01:49 -0600

    Added anlaytics.js to the index page.

--- js/script.js
+++ js/script.js
@@ -1,22 +1,4 @@
 $(function() {
-	analytics.initialize({
-	'Google Analytics' : 'UA-38271132-1',
-	'Mixpanel' : {
-					token:'16ed4d07e7761ffc4813bf58166a957f'
-				}
-	});
-
-	analytics.track("Visited Homepage");
-    analytics.pageview();
-
-	var link = $('.convert');
-
-	analytics.trackLink(contact, 'Clicked to Convert');
-	
-	var contact = $('#contact_us');
-	
-	analytics.trackLink(contact, 'Clicked to Contact');
-	
 	$('.carousel').carousel({
 		pause: null,
 		interval: 15000
