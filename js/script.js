0b8a2cd1a209ebbeb2c03a1bc84224fd83ac16e4
Nima Gardideh <nima@halfmoon.ws>
2013-02-07 13:21:03 -0500

    Fixed Analytics.

--- js/script.js
+++ js/script.js
@@ -1,19 +1,4 @@
 $(function() {
-	analytics.initialize({
-	'Google Analytics' : 'UA-38270530-1',
-	'Mixpanel' : {
-					token:'b9abac3f5a669c5e18fe739c2b1bbce6',
-				}
-	});
-
-	var link = $('.convert');
-
-	analytics.trackLink(contact, 'Clicked to Convert');
-	
-	var contact = $('#contact_us');
-	
-	analytics.trackLink(contact, 'Clicked to Contact');
-	
 	$('.carousel').carousel({
 		pause: null,
 		interval: 15000
